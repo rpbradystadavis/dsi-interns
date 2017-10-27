eabc18c66a6804307e4db86f5e338bddf025ce46
Jack Li <liyunzhejack@gmail.com>
2017-10-27 13:04:36 -0700

    test locat_section function with different dictionaries

--- testBySection.R
+++ testBySection.R
@@ -18,34 +18,42 @@ testfinder = function(text,dictionary){
 }
 
 # still not work properly for the paper which don't have such sections
-locate_section = function(x) 
+locate_section = function(xml, dictionary) 
   # only look for section of "study design" or "methods and materials"
   # input : one xml tree
   # output : texts of sections
 {
-  if(is.list(x))
-    x = x[[1]]
+  x = xml[[1]]
   header = findSectionHeaders(x)
   key = grep("study|method", names(header), ignore.case = TRUE, value = TRUE)
-  # keys = unlist(strsplit(key, ' '))
-  # keys = noquote(paste("\`", keys, "\`", sep = ""))
-  # names(keys) = NULL
-  # print(length(unlist(key)))
+  
   if(length(unlist(key)) < 4 & length(unlist(key)) > 0){
     section = getSectionText(x)
     section_names = names(section)
     indices = sapply(section_names, function(x) grepl(x, key, ignore.case = TRUE))
     section_text = section[indices]
     section_text = paste(section_text, sep = " ", collapse = " ")
-    tests = testfinder(section_text, testsDictionary)
+    tests = testfinder(section_text, dictionary)
     tests = unique(tests)
     return(tests)
   } else{
-    # print("else")
-    section = getSectionText(x)
-    section_text = paste(section_text, sep = " ", collapse = " ")
-    tests = testfinder(section_text, testsDictionary)
-    tests = unique(tests)
-    return(tests)
+    print("else")
   }
+}
+
+
+giveMeIndex = function(header)
+{
+  TF = grepl("study|method", names(header), ignore.case = TRUE)
+  if(length(which(TF==TRUE)) != 0)
+    return(1)
+  else
+    return(0)
+}
+giveMeSectionFile = function(headers)
+{
+  indices = sapply(headers, giveMeIndex)
+  real_indices = which(indices == 1)
+  sectionFiles = files[real_indices]
+  return(unlist(sectionFiles))
 }
